fd0e88f6422320b5053e7cef6bcdf7409858f01d
Vaimbhav <vaimbhav05@gmail.com>
2024-09-12 19:18:35 +0530

    First commit

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { useContext } from "react";
+import { Context } from "../../main";
+import { Navigate } from "react-router-dom";
+import HeroSection from "./HeroSection";
+import Demo from "./Demo";
+import PopularCategories from "./PopularCategories";
+import PopularCompanies from "./PopularCompanies";
+
+const Home = () => {
+  const { isAuthorized } = useContext(Context);
+  if (!isAuthorized) {
+    return <Navigate to={"/login"} />;
+  }
+  return (
+    <>
+      <section className="homePage page">
+        <HeroSection />
+        <Demo />
+        <PopularCategories />
+        <PopularCompanies />
+      </section>
+    </>
+  );
+};
+
+export default Home;
